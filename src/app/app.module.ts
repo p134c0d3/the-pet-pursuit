7b97410dbd25c259bf412949b8152ce104b06056
hslape <awinter71417@gmail.com>
2023-12-02 21:06:55 -0600

    created new post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NewPostComponent } from './new-post/new-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SignUpComponent
+    SignUpComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
