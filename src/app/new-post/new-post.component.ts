a3f7adfc95b1d394b1da4704c1c8a50382f05836
hslape <awinter71417@gmail.com>
2023-12-03 11:28:32 -0600

    changed form type; completed form input

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.scss']
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit {
 
+  ngOnInit(): void {}
+
+  newPost(form: NgForm) {
+
+    const petName = form.value.petName;
+    const petType = form.value.petType;
+    const sex = form.value.sex;
+    const location = form.value.location;
+    const description = form.value.description;
+    const firstName = form.value.firstName;
+    const lastName = form.value.lastName;
+    const email = form.value.email;
+    const phone = form.value.phone;
+
+  }
 }
