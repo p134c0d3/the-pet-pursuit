585c4a488bb90061d73ac779da16c5c6c62da50f
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-12-04 20:31:03 -0600

    added injectable. added signup method, added constructor

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from "@angular/core";
+
+
+
+@Injectable({providedIn:'root'})
+
+
+export class AuthService{
+  constructor(){}
+
+
+  signup(){
+
+  }
+}
+
+
+
+
+
+
+
+// https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
