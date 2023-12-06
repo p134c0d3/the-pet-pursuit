3ef9b04cbe935d299d2adab79712acb7942e2a91
Marcella44 <139720818+Marcella44@users.noreply.github.com>
2023-12-05 18:04:54 -0600

    Updated Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RequestListComponent } from './adoptionRequests/request-list/request-list.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
 
-    { path: 'home', component: AppComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'incomingApplications', component: RequestListComponent }
+
 
 ];
 
