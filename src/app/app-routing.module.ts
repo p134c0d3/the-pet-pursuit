de4695bc1f1e6c503157486eb2426bc384529261
Marcella44 <139720818+Marcella44@users.noreply.github.com>
2023-12-07 17:09:11 -0600

    Updated navbar and routing with incoming applications link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AdoptionApplicationListComponent } from './adoption-application-list/adoption-application-list.component';
 
 const routes: Routes = [
 
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'incomingApplications', component: AdoptionApplicationListComponent }
+
 ];
 
 @NgModule({
