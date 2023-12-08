20aed702f3d1f68a7c3c21e402946dcd0bde3b95
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-12-08 11:06:26 -0600

    added authentication code to login component ts

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-log-in',
@@ -8,20 +9,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  error:string=null
+  constructor(private router: Router, private authService:AuthService) { }
   ngOnInit() {
 
   }
 
   onLogin(form: NgForm) {
 
+    if(!form.valid){
+      return
+    }
+
     const email = form.value.email;
     const password = form.value.password;
+    this.authService.login(email, password).subscribe(
+      resData => {
+        this.router.navigate(['home']);
+        console.log(resData);
+        // isSuccessful=true
+       },
+       error =>{
+        console.log(error)
+        this.error='an error occured'
+       }
+       )
+
+
+
+
+
 
     console.log(form.value)
 
-    this.router.navigate(['home']);
+
+
+
+
     // this.router.navigate(['sign-in']);
 
     // this.authService.signupUser(email, password);
