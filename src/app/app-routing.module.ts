cd9e74edd41dd5dd9aca1288a4e251af592f141a
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-12-11 14:41:14 -0600

    Added auth guard and added to app routing module for future use

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AdoptionApplicationListComponent } from './adoption-application-list/ad
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
 
@@ -12,7 +13,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'nav-bar', component: NavBarComponent},
   { path: 'sign-up', component: SignUpComponent},
-  { path: 'log-in', component: LogInComponent}, 
+  { path: 'log-in', component: LogInComponent},
   { path: 'incomingApplications', component: AdoptionApplicationListComponent }
 ];
 
